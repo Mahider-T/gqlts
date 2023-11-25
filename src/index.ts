43bb4d31b276a3602e2377caeafffb592164657f
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-11-25 17:29:14 +0300

    Add nested queries

--- src/index.ts
+++ src/index.ts
@@ -8,17 +8,45 @@ import { books, authors, reviews } from "./_db.js";
 // console.log(books)
 const resolvers = {
     Query: {
-        book() {
+        books() {
             return books;
         },
-        author() {
+        book(_,args){
+            return books.find((oneBook)=> oneBook.id === args.id)
+        },
+        authors() {
             return authors;
         },
-        review() {
+        author(_,args) {
+            return authors.find((oneAuthor) => oneAuthor.id === args.id);
+        },
+        reviews() {
             return reviews;
-        }
-
+        },
+        review(_,args){
+            return reviews.find((oneReview) => oneReview.id === args.id);
+        },
+    },
+   //filter all reviews associated with a single book
+   Book: {
+    reviews(parent){
+        return reviews.filter((oneReview) => oneReview.book_id === parent.id);
     }
+   },
+   //filter all reviews of a single author 
+   Author: {
+    reviews(parent){
+        return reviews.filter((oneReview) => oneReview.author_id === parent.id)
+    }
+   },
+   Review: {
+       author(parent){
+           return authors.find((oneAuthor) => oneAuthor.id === parent.author_id);
+       },
+       book(parent){
+           return books.find((oneBook) => oneBook.id === parent.book_id);
+       }
+   }
 }
 // 
 const server = new ApolloServer({
--- src/schema.ts
+++ src/schema.ts
@@ -3,20 +3,29 @@ export const typeDefs = `#graphql
         id: ID!
         title: String!
         store: [String!]!
+        reviews: [Review!]
     }
     type Author {
         id: ID!
         name: String!
         verified: Boolean
+        reviews: [Review!]
     }
     type Review {
         id: ID!
         content: String!
         rating: Int!
+        book_id: String!
+        author_id: String!
+        author: Author!
+        book: Book!
     }
     type Query {
-        book: [Book]
-        author: [Author]
-        review: [Review] 
+        books: [Book]
+        book(id: ID!): Book 
+        authors: [Author]
+        author(id: ID!): Author 
+        reviews: [Review] 
+        review(id: ID!): Review 
     }
 `
